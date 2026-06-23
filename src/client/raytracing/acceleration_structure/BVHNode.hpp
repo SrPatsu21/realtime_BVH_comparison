64a5a5914dff9b53fe9553629f06b703d8e17ba3
SrPatsu21 <paulosergiopierdona@gmail.com>
2026-06-23 17:56:53 -0300

    node

---
+++ src/client/raytracing/acceleration_structure/BVHNode.hpp
@@ -0,0 +1,29 @@
+#include "AABB.hpp"
+#include <cstdint>
+
+struct BVHNode
+{
+    AABB bounds;
+
+    union
+    {
+        struct
+        {
+            uint32_t left;
+            uint32_t right;
+        };
+
+        struct
+        {
+            uint32_t firstPrimitive;
+            uint32_t primitiveCount;
+        };
+    };
+
+    bool leaf;
+
+    inline AABB& getBounds()
+    {
+        return bounds;
+    }
+};
